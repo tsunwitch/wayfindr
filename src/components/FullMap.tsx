7e84be7285324799215e6317e052aa5c4f29bc2f
Mateusz Nawieśniak <mateusz.nawiesniak@gmail.com>
2023-10-24 13:56:41 +0200

    Update FullMap.tsx

--- src/components/FullMap.tsx
+++ src/components/FullMap.tsx
@@ -17,18 +17,31 @@ export const FullMap = () => {
   }
 
   return (
-    <MapContainer
-      center={[position?.coords.latitude ?? 0, position?.coords.longitude ?? 0]}
-      zoom={100}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    <>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css"
       />
-      <Marker position={[49.7835438, 19.0589105]}>
-        <Popup>tutaj lukasz nadupia kod</Popup>
-      </Marker>
-    </MapContainer>
+      <link rel="stylesheet" href="leaflet-routing-machine.css" />
+      <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"></script>
+      <script src="leaflet-routing-machine.js"></script>
+
+      <MapContainer
+        center={[
+          position?.coords.latitude ?? 0,
+          position?.coords.longitude ?? 0,
+        ]}
+        zoom={100}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[49.7835438, 19.0589105]}>
+          <Popup>tutaj lukasz nadupia kod</Popup>
+        </Marker>
+      </MapContainer>
+    </>
   );
 };
