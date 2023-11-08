0a01f136e254368b8440e27486de6329df6101af
Mateusz Nawieśniak <mateusz.nawiesniak@gmail.com>
2023-11-08 16:13:15 +0100

    Added functions for drawing path
    uwuboys

--- src/components/FullMap.tsx
+++ src/components/FullMap.tsx
@@ -1,5 +1,46 @@
+import { LatLng } from "leaflet";
 import { useEffect, useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  Polyline,
+} from "react-leaflet";
+
+const response = await fetch(
+  "http://router.project-osrm.org/route/v1/driving/long,lat;long2,lat2?steps=true&annotations=true&geometries=geojson&overview=full"
+);
+
+async function getRoutePoints(
+  lat1: number,
+  long1: number,
+  lat2: number,
+  long2: number
+): Promise<Response> {
+  const response = await fetch(
+    `http://router.project-osrm.org/route/v1/driving/${long1},${lat1};${long2},${lat2}?steps=true&annotations=true&geometries=geojson&overview=full`
+  );
+
+  return response;
+}
+
+async function parseRoutePoints(response: Response) {
+  const router = (await response.json())["routes"][0]["geometry"][
+    "coordinates"
+  ];
+  const routepoints: any[] = [];
+
+  for (let i = 0; i < router.length; i++) {
+    const split = router[i]
+      .toString()
+      .replaceAll("[", "")
+      .replaceAll("]", "")
+      .split(",");
+
+    routepoints.push([parseFloat(split[0]), parseFloat(split[1])]);
+  }
+}
 
 export const FullMap = () => {
   const [position, setPosition] = useState<GeolocationPosition | null>(null);
@@ -18,14 +59,6 @@ export const FullMap = () => {
 
   return (
     <>
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css"
-      />
-      <link rel="stylesheet" href="leaflet-routing-machine.css" />
-      <script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"></script>
-      <script src="leaflet-routing-machine.js"></script>
-
       <MapContainer
         center={[
           position?.coords.latitude ?? 0,
@@ -38,6 +71,7 @@ export const FullMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <Polyline pathOptions={{ color: "red" }} positions={[]} />
         <Marker position={[49.7835438, 19.0589105]}>
           <Popup>tutaj lukasz nadupia kod</Popup>
         </Marker>
